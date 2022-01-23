102742225eef04c5fe0b56453a641ff55fd4026a
Lukasz Terpilowski <terpilowski.l@gmail.com>
2022-01-23 23:14:07 +0100

    First commit

---
+++ django/core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import MovieList,MovieDetail,PersonDetail,CreateVote,UpdateVote, MovieImageUpload, TopMovies
+
+app_name='core'
+urlpatterns = [
+    path('movies/',MovieList.as_view(),name='MovieList'),
+    path('movie/<int:pk>',MovieDetail.as_view(),name='MovieDetail'),
+    path('person/<int:pk>',PersonDetail.as_view(),name='PersonDetail'),
+    path('movie/<int:movie_id>/vote',CreateVote.as_view(),name='CreateVote'),
+    path('movie/<int:movie_id>/vote/<int:pk>',UpdateVote.as_view(),name='UpdateVote'),
+    path('movie/<int:movie_id>/image/upload/',MovieImageUpload.as_view(),name='MovieImageUpload'),
+    path('movie/top/',TopMovies.as_view(),name='TopMovies'),
+]
